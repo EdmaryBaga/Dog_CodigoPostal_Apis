b8182406075b1a70f605ab8199dcb540bc11b46e
Miguel <miguel.leon@gonet.us>
2019-06-05 18:26:10 -0500

    Adaptando a servicios

---
+++ app/src/main/java/com/bbva/intranet/baz/miguel/model/ServicesWs.kt
@@ -0,0 +1,13 @@
+package com.bbva.intranet.baz.miguel.model
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bbva.intranet.baz.R
+
+class ServicesWs : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_services_ws)
+    }
+}
